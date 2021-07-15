befdd270e1648bbc598aea4973fea58a08003cfa
Bruno <brunobsribeiro@gmail.com>
2021-07-14 21:04:14 -0300

    initial commit

---
+++ components/Subdivisao.jsx
@@ -0,0 +1,14 @@
+//importando o CSS do componente
+import styles from '../styles/Subdivisao.module.css';
+export default function Subdivisao(props) {
+  return (
+    <>
+      <div
+        style={{
+          backgroundColor: props.preta ? '#000' : '#fff',
+        }}
+        className={styles.subdivisao}
+      ></div>
+    </>
+  );
+}
