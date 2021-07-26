6b8f82b74d78b625ea6887cac80f50e217446cf0
Bruno <brunobsribeiro@gmail.com>
2021-07-26 20:44:47 -0300

    refacotr: pass prppperty

--- components/Linha.jsx
+++ components/Linha.jsx
@@ -2,6 +2,7 @@ import Subdivisao from './Subdivisao';
 import styles from '../styles/Linha.module.css';
 export default function Linha(props) {
   return (
+    //passando propriedade
     <div className={styles.linha}>
       <Subdivisao preta={props.preta}></Subdivisao>
       <Subdivisao preta={!props.preta}></Subdivisao>
